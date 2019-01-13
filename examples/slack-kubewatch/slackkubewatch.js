1d94fc9ca4b97850a567573fa0efc9202210d860
nea1 <neallidster@gmail.com>
2019-01-13 17:18:57 +0000

    kubeifying

--- examples/slack-kubewatch/slackkubewatch.js
+++ examples/slack-kubewatch/slackkubewatch.js
@@ -1,28 +1,32 @@
 let NATS = require('nats')
-let nats = NATS.connect({ 'json': true })
+let url = process.env.NATS_URL
+if (!url) {
+    url = "nats://localhost:4222"
+}
+let nats = NATS.connect({json: true, url})
 
 nats.subscribe('kube.event.watch', resp => {
-  if (resp instanceof NATS.NatsError) {
-    console.log(`could not subscribe to kube.watch ${resp}`)
-    return
-  }
-  let slackMsg = {
-    channel: 'CDNPXK2KT',
-    as_user: true,
-    attachments: [
-      {
-        fallback: `Namespace: ${resp.Object.involvedObject.namespace} Name: ${resp.Object.involvedObject.name} Kind: ${resp.Object.involvedObject.kind} Reason: ${resp.Object.reason} Message: ${resp.Object.message} Source: ${resp.Object.source.component}`,
-        mrkdwn_in: ['text', 'pretext', 'fields'],
-        color: '#33A2FF',
-        pretext: `:kube: *${resp.cluster}*`,
-        text: `*Namespace:* ${resp.Object.involvedObject.namespace}\n*Name:* ${resp.Object.involvedObject.name}\n*Kind:* ${resp.Object.involvedObject.kind}\n*Reason:* ${resp.Object.reason}\n*Message:* ${resp.Object.message}\n*Source:* ${resp.Object.source.component}`,
-        footer: `${resp.Object.lastTimestamp}`
-      }
-    ]
-  }
-  nats.requestOne('slack.chat.postMessage', slackMsg, {}, 3000, resp => {
     if (resp instanceof NATS.NatsError) {
-      console.log(`could not post slack msg ${resp}`)
+        console.log(`could not subscribe to kube.watch ${resp}`)
+        return
     }
-  })
+    let slackMsg = {
+        channel: 'CDNPXK2KT',
+        as_user: true,
+        attachments: [
+            {
+                fallback: `Namespace: ${resp.Object.involvedObject.namespace} Name: ${resp.Object.involvedObject.name} Kind: ${resp.Object.involvedObject.kind} Reason: ${resp.Object.reason} Message: ${resp.Object.message} Source: ${resp.Object.source.component}`,
+                mrkdwn_in: ['text', 'pretext', 'fields'],
+                color: '#33A2FF',
+                pretext: `:kube: *${resp.cluster}*`,
+                text: `*Namespace:* ${resp.Object.involvedObject.namespace}\n*Name:* ${resp.Object.involvedObject.name}\n*Kind:* ${resp.Object.involvedObject.kind}\n*Reason:* ${resp.Object.reason}\n*Message:* ${resp.Object.message}\n*Source:* ${resp.Object.source.component}`,
+                footer: `${resp.Object.lastTimestamp}`
+            }
+        ]
+    }
+    nats.requestOne('slack.chat.postMessage', slackMsg, {}, 3000, resp => {
+        if (resp instanceof NATS.NatsError) {
+            console.log(`could not post slack msg ${resp}`)
+        }
+    })
 })
